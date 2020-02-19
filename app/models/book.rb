fee55e30de7e0b2d73fdf64131b196f5cf0f05e8
adriaanbd <adriaanbd@gmail.com>
2020-02-18 22:45:33 -0500

    validations and downcase

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :category, presence: true
+    before_save { title.downcase! }
+    before_save { category.downcase! }
+    before_save { author.downcase! }
 end
